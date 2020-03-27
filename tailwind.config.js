d6d3230b67ea833bf0f4761f0984917bee680081
Matan Kushner <hello@matchai.dev>
2020-03-26 20:25:41 -0400

    Add projects section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   theme: {
+    screens: {
+      print: { raw: "print" }
+    },
     extend: {
       maxWidth: {
         letter: "8.5in"
       }
     }
+  },
+  variants: {
+    textColor: ["responsive", "hover", "focus", "group-hover"]
   }
 };
